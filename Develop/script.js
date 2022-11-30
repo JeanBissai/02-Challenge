738395360b494c648953a4dcf7781da58f615096
Your Name <you@example.com>
2022-11-29 20:49:47 -0700

    init

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+const randomUnicode = () => {
+  const Unicode = [
+    "U+2022",
+    "U-2021",
+    "U+2020"
+    
+];
+return Unicode[Math.floor(Math.random() * Unicode.length)];
+}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector("password");
 
   passwordText.value = password;
 
